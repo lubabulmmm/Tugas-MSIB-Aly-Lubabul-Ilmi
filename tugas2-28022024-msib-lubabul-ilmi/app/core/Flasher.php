3c6d9f9c2644a5dbb5d3c2ec1631fbe3086ae30e
lubabulmmm <lubabul.ilmi16@gmail.com>
2024-03-16 23:07:17 +0700

    update
    update edit and flasher

--- tugas2-28022024-msib-lubabul-ilmi/app/core/Flasher.php
+++ tugas2-28022024-msib-lubabul-ilmi/app/core/Flasher.php
@@ -9,16 +9,42 @@ class Flasher
             'tipe' => $tipe
         ];
     }
+
     public static function flash()
     {
-        if (isset($_SESSION['flash'])) {
-            echo '<div class="alert alert-' . $_SESSION['flash']['tipe'] . ' alert-dismissible fade show" role="alert">
-              Artikel <strong>' . $_SESSION['flash']['pesan'] . '</strong> ' . $_SESSION['flash']['aksi'] . '
-              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-              <span aria-hidden="true">×</span>
-               </button>
-                    </div>';
+        if (isset ($_SESSION['flash'])) {
+            echo '<div class="bg-' . self::getColor($_SESSION['flash']['tipe']) . '-100 border border-' . self::getColor($_SESSION['flash']['tipe']) . '-400 text-' . self::getColor($_SESSION['flash']['tipe']) . '-700 px-4 py-3 rounded relative" role="alert">
+                    <strong class="font-bold">' . $_SESSION['flash']['pesan'] . '</strong>
+                    <span class="block sm:inline">' . $_SESSION['flash']['aksi'] . '</span>
+                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                        <svg class="fill-current h-6 w-6 text-' . self::getColor($_SESSION['flash']['tipe']) . '-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <title>Close</title>
+                            <path fill-rule="evenodd" d="M14.293 5.293a1 1 0 0 1 1.414 1.414L11.414 10l4.293 4.293a1 1 0 1 1-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 1 1-1.414-1.414L8.586 10 4.293 5.707a1 1 0 0 1 1.414-1.414L10 8.586l4.293-4.293z" clip-rule="evenodd"/>
+                        </svg>
+                    </span>
+                </div>';
             unset($_SESSION['flash']);
+        }
+    }
+
+    public static function getColor($tipe)
+    {
+        switch ($tipe) {
+            case 'success':
+                return 'green';
+                break;
+            case 'danger':
+                return 'red';
+                break;
+            case 'warning':
+                return 'yellow';
+                break;
+            case 'info':
+                return 'blue';
+                break;
+            default:
+                return 'gray';
+                break;
         }
     }
 }
--- tugas2-28022024-msib-lubabul-ilmi/app/views/wisata/detail.php
+++ tugas2-28022024-msib-lubabul-ilmi/app/views/wisata/detail.php
@@ -1,75 +1,68 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+class Wisata_model
+{
+    private $table = 'wisata';
+    private $db;
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman
-        <?= $data['judul']; ?>
-    </title>
-</head>
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
 
-<body class="bg-gray-900">
+    public function getAllWisata()
+    {
+        $this->db->query("SELECT * FROM " . $this->table);
+        return $this->db->resultAll();
+    }
 
+    public function getWisataById($id)
+    {
+        $this->db->query("SELECT * FROM " . $this->table . ' WHERE id=:id');
+        $this->db->bind('id', $id);
+        return $this->db->resultSingle();
+    }
+    public function buatWisata($data)
+    {
+        $query = "INSERT INTO wisata VALUES ('', :wisata, :deskripsi, :alamat, :HTM )";
+        $this->db->query($query);
+        $this->db->bind('wisata', $data['wisata']);
+        $this->db->bind('deskripsi', $data['deskripsi']);
+        $this->db->bind('alamat', $data['alamat']);
+        $this->db->bind('HTM', $data['HTM']);
+        $this->db->execute();
+        return $this->db->rowCount();
+    }
 
-    <section class="bg-white dark:bg-gray-900">
-        <div class="max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-            <img class="h-auto max-w-lg mx-auto" src="<?= BASE_URL; ?>/img/batu.png" alt="image description">
+    public function hapusWisata($id)
+    {
+        $query = "DELETE FROM wisata WHERE id = :id";
+        $this->db->query($query);
+        $this->db->bind('id', $id);
+        $this->db->execute();
+        return $this->db->rowCount();
 
+    }
 
+    public function ubahWisata($data)
+    {
+        $query = "UPDATE wisata SET wisata=:wisata, deskripsi=:deskripsi, alamat=:alamat, HTM=:HTM  WHERE id = :id     ";
+        $this->db->query($query);
+        $this->db->bind('wisata', $data['wisata']);
+        $this->db->bind('deskripsi', $data['deskripsi']);
+        $this->db->bind('alamat', $data['alamat']);
+        $this->db->bind('HTM', $data['HTM']);
+        $this->db->bind('id', $data['id']);
 
+        $this->db->execute();
+        return $this->db->rowCount();
+    }
 
-
-
-        </div>
-    </section>
-    <section class="bg-white dark:bg-gray-900">
-        <div class="max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-
-            <form class="max-w-sm mx-auto">
-                <div class="mb-5">
-                    <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama
-                        Wisata</label>
-                    <input type="text" aria-label="large-input"
-                        class="mb-5 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        value="<?= $data['wisata']['wisata']; ?>" required></input>
-                </div>
-                <div class="mb-5">
-                    <label for="large-input"
-                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Deskripsi</label>
-                    <input type="text" aria-label="large-input"
-                        class="mb-5 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        value="<?= $data['wisata']['deskripsi']; ?>" required></input>
-                </div>
-                <div class="mb-5">
-                    <label for="large-input"
-                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Alamat</label>
-                    <input type="text" aria-label="large-input"
-                        class="mb-5 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        value="<?= $data['wisata']['alamat']; ?>" required></input>
-                </div>
-                <div class="mb-5">
-                    <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Harga
-                        Masuk</label>
-                    <input type="text" aria-label="large-input"
-                        class="mb-5 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        value="<?= $data['wisata']['HTM']; ?>" required></input>
-                </div>
-                <a href="<?= BASE_URL; ?>/wisata" type="button"
-                    class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-red-500 rounded-lg focus:ring-4 focus:ring-blue-200 hover:bg-red-400">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M13 5H1m0 0 4 4M1 5l4-4" />
-                    Kembali
-                </a>
-                <a href="<?= BASE_URL; ?>/wisata/ubah" type="button"
-                    class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-yellow-500 rounded-lg focus:ring-4 focus:ring-blue-200 hover:bg-yellow-400">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M13 5H1m0 0 4 4M1 5l4-4" />
-                    Ubah
-                </a>
-
-            </form>
-
-</body>
-
-</html>
+    public function cariDataWisata()
+    {
+        $keyword = $_POST['keyword'];
+        $query = "SELECT * FROM wisata WHERE wisata LIKE :keyword";
+        $this->db->query($query);
+        $this->db->bind('keyword', "%$keyword%");
+        return $this->db->resultAll();
+    }
+}
